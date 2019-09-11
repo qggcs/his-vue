4a755f40380ede8140576c6dcb3c3d644dd233c2
qggcs <ajb459684460@163.com>
2019-09-11 11:09:49 +0800

    modify 2019.9.10

--- src/components/doctor/Consult.vue
+++ src/components/doctor/Consult.vue
@@ -1,15 +1,121 @@
 <template>
   <div>
-    <center>
-      <h1>这是看诊页面</h1>
-    </center>
+    <el-tag>病史内容:</el-tag>
+    <el-form ref="form" :model="medicalRecord" label-width="80px">
+      <el-form-item label="主诉" v-model="medicalRecord.description">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="现病史" v-model="medicalRecord.currentIllnessHistory">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="治疗情况" v-model="medicalRecord.currentTreatment">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="既往史" v-model="medicalRecord.pastHistory">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="过敏史" v-model="medicalRecord.allergyHistory">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="体格检查" v-model="medicalRecord.physicalExamination">
+        <el-input type="textarea"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-tag>评估/诊断:</el-tag>
+    <el-card style="height:300px">
+      <div slot="header">
+        <span style="font-size:14px">西医诊断</span>
+        <el-button style="float: right" type="danger" size="small">删除</el-button>
+        <el-button style="float: right" type="primary" size="small" @click="dialogVisible = true">添加</el-button>
+      </div>
+      <el-table
+        :data="diagnoses"
+        tooltip-effect="dark"
+        ref="mulTable"
+        @selection-change="handleSelectionChange"
+        height="250px"
+      >
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column prop="icdCode" width="110"></el-table-column>
+        <el-table-column prop="diseaseName" width="500"></el-table-column>
+        <el-table-column prop="diseaseDate" width="110"></el-table-column>
+      </el-table>
+    </el-card>
+
+    <el-dialog title="诊断信息" :visible.sync="dialogVisible" width="20%">
+      <el-form>
+        <el-form-item label="诊断疾病">
+          <el-select v-model="diagnose.diseaseId">
+            <el-option
+              v-for="item in diseases"
+              :key="item.diseaseId"
+              :label="item.diseaseName"
+              :value="item.diseaseId"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="发病日期">
+          <el-date-picker v-model="diagnose.diseaseDate" type="datetime" placeholder="选择日期时间"></el-date-picker>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addDiagnose">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { request } from "../../request";
 export default {
   data() {
-    return {};
+    return {
+      register: JSON.parse(sessionStorage.getItem("register")),
+      medicalRecord: {
+        description: "",
+        currentIllnessHistory: "",
+        currentTreatment: "",
+        pastHistory: "",
+        allergyHistory: "",
+        physicalExamination: ""
+      },
+      diagnoses: [],
+      diagnose: {
+        medicalRecordNumber: "",
+        registerId: "",
+        diseaseId: "",
+        diagnosticType: "1",
+        diseaseDate: "",
+        diagnosticStatus: "1"
+      },
+      diseases: [],
+      dialogVisible: false
+    };
+  },
+  methods: {
+    toggleSelection() {
+      this.$refs.mulTable.clearSelection();
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    getAllDisease() {
+      request({
+        url: "/diseases",
+        method: "get"
+      }).then(res => {
+        this.diseases = res.data.diseases;
+      });
+    },
+    addDiagnose() {
+      this.diagnose.medicalRecordNumber = this.register.medicalRecordNumber;
+      this.diagnose.registerId = this.register.registerId;
+      dialogVisible = false;
+    }
+  },
+  created() {
+    this.getAllDisease();
   }
 };
 </script>
--- src/components/doctor/Main.vue
+++ src/components/doctor/Main.vue
@@ -1,27 +1,57 @@
 <template>
   <div>
     <el-container>
-      <el-aside width="300px"></el-aside>
+      <el-aside width="300px">
+        <el-card>
+          <div slot="header" class="clearfix">
+            <span>未诊患者</span>
+          </div>
+          <el-table
+            height="250"
+            ref="regTable"
+            :data="registered"
+            highlight-current-row
+            @current-change="handleCurrentChange"
+          >
+            <el-table-column label="病历号" prop="medicalRecordNumber" width="100"></el-table-column>
+            <el-table-column label="姓名" prop="name" width="100"></el-table-column>
+            <el-table-column label="年龄" prop="year" width="50"></el-table-column>
+          </el-table>
+        </el-card>
+
+        <el-card>
+          <div slot="header" class="clearfix">
+            <span>已诊患者</span>
+          </div>
+          <el-table
+            ref="diagTable"
+            :data="diagnosed"
+            width="100%"
+            highlight-current-row
+            @current-change="handleCurrentChange"
+            disabled
+          >
+            <el-table-column label="病历号" prop="medicalRecordNumber" width="100"></el-table-column>
+            <el-table-column label="姓名" prop="name" width="100"></el-table-column>
+            <el-table-column label="年龄" prop="year" width="50"></el-table-column>
+          </el-table>
+        </el-card>
+      </el-aside>
       <el-container>
         <el-header
           style="padding: 0px;display:flex;justify-content:space-between;align-items: center"
         >
           <el-menu
             mode="horizontal"
-            @select="handleSelect"
             background-color="#b3c0d1"
             text-color="#000000"
             active-text-color="#606266"
           >
             <el-menu-item index="1">
-              <router-link to="/home/main/consult" tag="span">
-              病历首页
-              </router-link>
+              <router-link to="/home/main/consult" tag="span">病历首页</router-link>
             </el-menu-item>
             <el-menu-item index="2">
-              <router-link to="/home/main/Prescribe" tag="span">
-                成药处方
-              </router-link>
+              <router-link to="/home/main/Prescribe" tag="span">成药处方</router-link>
             </el-menu-item>
             <el-menu-item index="3">草药处方</el-menu-item>
             <el-menu-item index="4">费用查询</el-menu-item>
@@ -36,9 +66,41 @@
 </template>
 
 <script>
+import { request } from "../../request";
 export default {
   data() {
-    return {};
+    return {
+      doctor: JSON.parse(sessionStorage.getItem("user")),
+      registered: [],
+      diagnosed: [],
+      currentRow: []
+    };
+  },
+  methods: {
+    setCurrent(row) {
+      this.$refs.singleTable.setCurrentRow(row);
+    },
+    handleCurrentChange(val) {
+      this.currentRow = val;
+    }
+  },
+  created() {
+    request({
+      url: "/patients",
+      method: "get",
+      params: {
+        doctorId: 1
+      }
+    }).then(res => {
+      this.registered = res.data.registered;
+      this.diagnosed = res.data.diagnosed;
+    });
+  },
+  watch: {
+    currentRow: function(newVal, oldVal) {
+      sessionStorage.removeItem("register");
+      sessionStorage.setItem("register", JSON.stringify(newVal));
+    }
   }
 };
 </script>
